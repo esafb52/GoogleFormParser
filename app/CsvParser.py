e85d48d8dfa896b91ac04fd03263e22fcee54b70
masiha <esafb52@gmail.com>
2021-01-01 13:23:52 +0330

    implement app logic

---
+++ app/CsvParser.py
@@ -0,0 +1,78 @@
+import csv
+import os
+from shutil import copyfile
+
+
+def clean_extra_char(input_text):
+    return str(input_text) \
+        .replace('u200e', ' ') \
+        .replace('u200c', ' ') \
+        .replace('\\', '') \
+        .replace('\n', ' ')
+
+
+def save_answer(file_name):
+    students = []
+    lst_res = []
+    new_student_mode = False
+    counter = 0
+
+    input_file = 'static/uploads/' + file_name
+    last_result_from_input_file = 'static/uploads/' + 'last_result.csv'
+    copyfile(input_file, last_result_from_input_file)
+    out_path = 'static/uploads/answer.txt'
+    out_file = open(out_path, mode='w', encoding='utf-8')
+    if not os.path.exists(input_file):
+        return []
+    with open(input_file, encoding='utf-8') as csv_file:
+        answers = csv.reader(csv_file, delimiter=',', quotechar='"')
+        for answer in answers:
+            for item in answer:
+                result = clean_extra_char(item)
+                if 'گرینویچ' in result:
+                    counter = 0
+                    out_file.write('.' * 50)
+                    new_student_mode = True
+                    continue
+                if new_student_mode:
+                    students.append(item)
+                    lst_res.append('دانش آموز جدید' + item)
+                    new_student_mode = False
+                if result:
+                    counter += 1
+                    final_answer = ' پاسخ سوال ' + str(counter) + ' : ' + result
+                    out_file.write(final_answer + '\n')
+                    lst_res.append(final_answer)
+                    # print(final_answer)
+        out_file.write('\n\n' + 'اسامی شرکت کنندگان آزمون' + '\n')
+        counter = 0
+        for name in students:
+            counter += 1
+            out_file.write(str(counter) + ' : ' + name + '\n')
+        out_file.close()
+
+
+def get_form_result():
+    lst_res = []
+    new_student_mode = False
+    counter = 0
+    input_file = 'static/uploads/last_result.csv'
+    if not os.path.exists(input_file):
+        return []
+    with open(input_file, encoding='utf-8') as csv_file:
+        answers = csv.reader(csv_file, delimiter=',', quotechar='"')
+        for answer in answers:
+            for item in answer:
+                result = clean_extra_char(item)
+                if 'گرینویچ' in result:
+                    counter = 0
+                    new_student_mode = True
+                    continue
+                if new_student_mode:
+                    lst_res.append('@@@')
+                    new_student_mode = False
+                if result:
+                    counter += 1
+                    final_answer = ' پاسخ سوال ' + str(counter) + ' : ' + result
+                    lst_res.append(final_answer)
+    return lst_res
---
+++ app/app.py
@@ -0,0 +1,56 @@
+import os
+from flask import Flask, flash, redirect, request, send_from_directory, current_app
+from flask import render_template
+from werkzeug.utils import secure_filename
+from CsvParser import save_answer, get_form_result
+
+UPLOAD_FOLDER = 'static/uploads'
+ALLOWED_EXTENSIONS = {'csv'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = "das'@fdgsdf#345r43kl34k53#$%#$^#^#634dja;shasdeasdadas"
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def file_upload():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part', 'danger')
+            return redirect('/')
+        file = request.files['file']
+        if file.filename == '':
+            flash('فایلی انتخاب نشده است ', 'danger')
+            return redirect('/')
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            filename.replace(' ', '_')  # no space in filenames! because we will call them as command line arguments
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
+            save_answer(filename)
+            flash('فایل با موفقیت تبدیل شد ', 'info')
+            return redirect('/')
+
+
+@app.route('/')
+def index():
+    answers = get_form_result()
+    return render_template('index.html', data={'answers': answers})
+
+
+@app.route('/download', methods=['GET'])
+def download():
+    uploads = os.path.join(current_app.root_path, app.config['UPLOAD_FOLDER'])
+    filename = 'answer.txt'
+    if os.path.exists(uploads + '/' + filename):
+        return send_from_directory(directory=uploads, filename=filename)
+    flash('ابتدا پاسخ آزمون را آپلود کنید ', 'danger')
+    return redirect("/")
+
+
+if __name__ == '__main__':
+    app.run("0.0.0.0", 5000, debug=False)
